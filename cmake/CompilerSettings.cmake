c265a64e37f74285615627544cbd5260411d221d
abelbarreira <abelbarreira@hotmail.com>
2025-05-03 16:33:32 +0200

    build: configured status bar to select build variants and added MinSizeRel and RelWithDebInfo

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -1,6 +1,3 @@
-# Restrict the user to only use Debug and Release configurations
-set(CMAKE_CONFIGURATION_TYPES "Debug;Release" CACHE STRING "" FORCE)
-
 message(STATUS "CompilerSettings: Build type: ${CMAKE_BUILD_TYPE}")
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
@@ -16,9 +13,13 @@ function(apply_compiler_settings target options)
   target_compile_options(${target} ${options}
     # GNU: GCC, Clang (on Linux, macOS, or MinGW on Windows)
     $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:Debug>>:-Wall -Wextra -Wpedantic -g -O0>
+    $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:MinSizeRel>>:-DNDEBUG -Os>
+    $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:RelWithDebInfo>>:-DNDEBUG -g -O2>
     $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:Release>>:-DNDEBUG -flto -O3>
     # MSVC:  Microsoft Visual Studio MSVC compiler (on Windows)
     $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<CONFIG:Debug>>:/W4 /Zi /Od>
+    $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<CONFIG:MinSizeRel>>:/DNDEBUG /O1>
+    $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<CONFIG:RelWithDebInfo>>:/DNDEBUG /Zi /O2>
     $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<CONFIG:Release>>:/DNDEBUG /GL /LTCG /O2>
   )
 endfunction()
